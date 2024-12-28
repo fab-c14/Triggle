a6aab9c78c6ed7c53f8d4bd475bfe29481bdb0d1
Faisal Bhat <plesim18@gmail.com>
2024-12-28 10:31:52 +0000

    intial updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from "react";
+import Board from "./components/Board";
+import './index.css';
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="App text-center p-6">
+      <h1 className="font-lobster text-4xl text-blue-600">
+        Welcome to Triggle
+      </h1>
+      <p className="font-openSans text-lg text-gray-600 my-4">
+        Create triangles and outsmart your opponent!
       </p>
-    </>
-  )
-}
+     
+<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Button
+</button>
+    
+      <Board />
+    </div>
+  );
+};
 
-export default App
+export default App;
