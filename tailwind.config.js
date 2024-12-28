6b2e1da7f9790187341560e62ae68ccdb35b92c6
Faisal Bhat <plesim18@gmail.com>
2024-12-28 10:19:48 +0000

    intial setup

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        robotoMono: ["Roboto Mono", "monospace"],
+        lobster: ["Lobster", "cursive"],
+        montserrat: ["Montserrat", "sans-serif"],
+        openSans: ["Open Sans", "sans-serif"],
+        bebas: ["Bebas Neue", "cursive"],
+        fredoka: ["Fredoka", "sans-serif"],
+        playfair: ["Playfair Display", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
